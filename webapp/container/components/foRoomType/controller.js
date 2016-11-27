3ebbc4cad7080e07d93930e48a386f58d7aacd88
yadi <yadi.priyadi@inmagine.com>
2016-11-28 00:22:12 +0700

    room type module

--- webapp/container/components/foRoomType/controller.js
+++ webapp/container/components/foRoomType/controller.js
@@ -1,8 +1,7 @@
-
 var userController = angular.module('app', []);
 userController
 .controller('FoRoomTypeCtrl',
-function($scope, $state, $sce, customerService, DTOptionsBuilder, DTColumnBuilder, $localStorage, $compile, $rootScope, API_URL) {
+function($scope, $state, $sce, roomTypeService, DTOptionsBuilder, DTColumnBuilder, $localStorage, $compile, $rootScope,API_URL) {
 
     $scope.el = [];
     $scope.el = $state.current.data;
@@ -11,6 +10,224 @@ function($scope, $state, $sce, customerService, DTOptionsBuilder, DTColumnBuilde
     $scope.buttonDelete = false;
     for (var i=0;i<$scope.el.length;i++){
         $scope[$scope.el[i]] = true;
+    }
+    $scope.users = []
+
+    $scope.role = {
+        selected: []
+    };
+
+    $scope.id = '';
+    $scope.roomtype = {
+        id: '',
+        name: '',
+        active: ''
+    }
+    $scope.filterVal = {
+        search: ''
+    }
+    $scope.trustAsHtml = function(value) {
+        return $sce.trustAsHtml(value);
+    };
+    $scope.selected = {
+        roomtype: {}
+    }
+
+    $scope.arrActive = [
+        {
+            id: 1,
+            name: 'Yes'
+        },
+        {
+            id: 0,
+            name: 'No'
+        }
+    ]
+
+    $scope.roomtypes = []
+    roomTypeService.get()
+    .then(function(data){
+        $scope.roomtypes = data.data
+    })
+
+    /*START AD ServerSide*/
+    $scope.dtInstance = {} //Use for reloadData
+    $scope.actionsHtml = function(data, type, full, meta) {
+        $scope.roomtypes[data] = {id:data};
+        var html = ''
+        if ($scope.el.length>0){
+            html = '<div class="btn-group btn-group-xs">'
+            if ($scope.el.indexOf('buttonUpdate')>-1){
+                html +=
+                '<button class="btn btn-default" ng-click="update(roomtypes[\'' + data + '\'])">' +
+                '   <i class="fa fa-edit"></i>' +
+                '</button>&nbsp;' ;
+            }
+            if ($scope.el.indexOf('buttonDelete')>-1){
+                html+='<button class="btn btn-default" ng-click="delete(roomtypes[\'' + data + '\'])" )"="">' +
+                '   <i class="fa fa-trash-o"></i>' +
+                '</button>';
+            }
+            html += '</div>'
+        }
+        return html
+    }
+
+    $scope.createdRow = function(row, data, dataIndex) {
+        // Recompiling so we can bind Angular directive to the DT
+        $compile(angular.element(row).contents())($scope);
+    }
+
+    $scope.dtOptions = DTOptionsBuilder.newOptions()
+    .withOption('ajax', {
+        url: API_URL+'/apifo/getRoomTypes',
+        type: 'GET',
+        headers: {
+            "authorization":  'Basic ' + $localStorage.mediaToken
+        },
+        data: function (data) {
+            data.customSearch = $scope.filterVal.search;
+        }
+    })
+    .withDataProp('data')
+    .withOption('processing', true)
+    .withOption('serverSide', true)
+    .withOption('bLengthChange', false)
+    .withOption('bFilter', false)
+    .withPaginationType('full_numbers')
+    .withDisplayLength(10)
+
+    .withOption('createdRow', $scope.createdRow);
+
+    $scope.dtColumns = [];
+    if ($scope.el.length>0){
+        $scope.dtColumns.push(DTColumnBuilder.newColumn('id').withTitle('Action').notSortable()
+        .renderWith($scope.actionsHtml).withOption('width', '10%'))
+    }
+    $scope.dtColumns.push(
+        DTColumnBuilder.newColumn('name').withTitle('Room Name'),
+        DTColumnBuilder.newColumn('active').withTitle('Active')
+    );
+
+    $scope.filter = function(type,event) {
+        if (type == 'search'){
+            if (event.keyCode == 13){
+                $scope.dtInstance.reloadData(function(obj){
+                    // console.log(obj)
+                }, false)
+            }
+        }
+        else {
+            $scope.dtInstance.reloadData(function(obj){
+                // console.log(obj)
+            }, false)
+        }
+    }
+
+    /*END AD ServerSide*/
+
+    $scope.openQuickView = function(state){
+        if (state == 'add'){
+            $scope.clear()
+        }
+        $('#form-input').modal('show')
+    }
+
+    $scope.submit = function(){
+        // console.log($scope.contract)
+        if ($scope.roomtype.id.length==0){
+            //exec creation
+
+            $scope.roomtype.name = $scope.roomtype.name;
+            $scope.roomtype.active = $scope.selected.roomtype.selected.id;
+
+            roomTypeService.create($scope.roomtype)
+            .then(function (result){
+                    $('#form-input').modal('hide')
+                    $scope.dtInstance.reloadData(function(obj){
+                        console.log(obj)
+                    }, false)
+                    $('body').pgNotification({
+                        style: 'flip',
+                        message: 'Success Insert '+$scope.roomtype.name,
+                        position: 'top-right',
+                        timeout: 2000,
+                        type: 'success'
+                    }).show();
+
+            },
+            function (err){
+                $('#form-input').pgNotification({
+                    style: 'flip',
+                    message: 'Error Insert: '+err.desc.code,
+                    position: 'top-right',
+                    timeout: 2000,
+                    type: 'danger'
+                }).show();
+            })
+
+        }
+        else {
+            //exec update
+            $scope.roomtype.name = $scope.roomtype.name;
+            $scope.roomtype.active = $scope.selected.roomtype.selected.id;
+
+            roomTypeService.update($scope.roomtype)
+            .then(function (result){
+                if (result.status = "200"){
+                    console.log('Success Update')
+                    $('#form-input').modal('hide')
+                    $scope.dtInstance.reloadData(function(obj){
+                        console.log(obj)
+                    }, false)
+                }
+                else {
+                    console.log('Failed Update')
+                }
+            })
+        }
+    }
+
+    $scope.update = function(obj){
+        $('#form-input').modal('show');
+
+        roomTypeService.get(obj.id)
+        .then(function(result){
+        console.log(result)
+            $scope.roomtype.id = result.data[0].room_type_id
+            $scope.roomtype.name = result.data[0].room_type_name
+            $scope.roomtype.active = result.data[0].room_type_active
+            $scope.selected.roomtype.selected = {name: result.data[0].room_type_active == 1 ? 'Yes' : 'No' , id: result.data[0].room_type_active};
+        })
+    }
+
+    $scope.delete = function(obj){
+        $scope.roomtype.id = obj.id;
+        $('#modalDelete').modal('show')
+    }
+
+    $scope.execDelete = function(){
+        roomTypeService.delete($scope.roomtype)
+        .then(function (result){
+            if (result.status = "200"){
+                console.log('Success Update')
+                $('#form-input').modal('hide')
+                $scope.dtInstance.reloadData(function(obj){
+                    console.log(obj)
+                }, false)
+            }
+            else {
+                console.log('Failed Update')
+            }
+        })
+    }
+
+    $scope.clear = function(){
+        $scope.roomtype = {
+            id: '',
+            name: '',
+            active: ''
+        }
     }
 
 })
