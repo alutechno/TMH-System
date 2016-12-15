01b4eec5929ecfe65b440cec5b1befbf5d4a57ed
ricky yuliadi <ricky.yuliadi@gmail.com>
2016-12-15 12:37:05 +0700

    add api post query

--- routes/apiSql.js
+++ routes/apiSql.js
@@ -14,5 +14,11 @@ module.exports = function(connection,jwt){
             res.send(JSON.stringify({err:err,rows:rows,fields:fields}))
         });
     });
+	app.post('/query', function (req, res) {
+		console.log(req.body)
+		connection(req.body.query, req.body.values,function(err, rows, fields) {
+            res.send(JSON.stringify({err:err,rows:rows,fields:fields}))
+        });
+    });
     return app;
 }
