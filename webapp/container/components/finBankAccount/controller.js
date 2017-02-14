aed2073232b2e74abc4c11c61282e208014282cd
Danu Permadi <danu@alutechno.io>
2017-02-14 07:10:22 +0700

    bank account

--- webapp/container/components/finBankAccount/controller.js
+++ webapp/container/components/finBankAccount/controller.js
@@ -12,10 +12,18 @@ function($scope, $state, $sce, queryService, DTOptionsBuilder, DTColumnBuilder, 
     for (var i=0;i<$scope.el.length;i++){
         $scope[$scope.el[i]] = true;
     }
-    var qstring = "select a.*,b.name status_name from mst_department a, "+
-        "(select * from table_ref where table_name = 'ref_product_category' and column_name = 'status') b "+
-        "where a.status = b.value "+
-        "and a.status != '2' "
+    var qstring = 'select a.id, a.code, a.name, a.status, a.short_name, a.description,a.address1, a.address2, a.account_type, '+
+    	'a.bank_id, a.bank_account, a.currency_id, a.gl_account_id, a.ap_clearance_account_id, a.ar_clearance_account_id, '+
+        'b.name bank_name, c.name currency_name, d.code gl_account_code, d.name gl_account_name,g.name account_type_name, '+
+        'e.code ap_account_code, e.name ap_account_name, f.code ar_account_code, f.name ar_account_name '+
+     'from mst_cash_bank_account a '+
+     'left join mst_cash_bank b on a.bank_id = b.id '+
+     'left join ref_currency c on a.currency_id= c.id '+
+     'left join mst_ledger_account d on a.gl_account_id = d.id '+
+     'left join mst_ledger_account e on a.ap_clearance_account_id = e.id '+
+     'left join mst_ledger_account f on a.ar_clearance_account_id = f.id '+
+     'left join (select * from table_ref where table_name = \'mst_cash_bank_account\' and column_name = \'account_type\') g on a.account_type = g.value '+
+     ' where a.status!=2 '
     var qwhere = ''
     $scope.users = []
 
@@ -35,7 +43,13 @@ function($scope, $state, $sce, queryService, DTOptionsBuilder, DTColumnBuilder, 
     }
 
     $scope.selected = {
-        dep: {}
+        dep: {},
+        account_type: {},
+        bank: {},
+        currency: {},
+        gl_account: {},
+        ap_clearance: {},
+        ar_clearance: {}
     }
 
     $scope.arrActive = [
@@ -48,6 +62,34 @@ function($scope, $state, $sce, queryService, DTOptionsBuilder, DTColumnBuilder, 
             name: 'No'
         }
     ]
+
+    $scope.account_type = []
+    queryService.get('select * from table_ref where table_name = \'mst_cash_bank_account\' and column_name = \'account_type\' order by id',undefined)
+    .then(function(data){
+        $scope.account_type = data.data
+    })
+
+    $scope.currency = []
+    queryService.get('select * from ref_currency order by id ',undefined)
+    .then(function(data){
+        $scope.currency = data.data
+    })
+
+    $scope.gl_account = []
+    queryService.get('select * from mst_ledger_account order by id ',undefined)
+    .then(function(data){
+        $scope.gl_account = data.data
+
+    })
+
+    $scope.bank = []
+    queryService.get('select * from mst_cash_bank order by id ',undefined)
+    .then(function(data){
+        $scope.bank = data.data
+
+    })
+
+
 
     $scope.filterVal = {
         search: ''
@@ -88,7 +130,7 @@ function($scope, $state, $sce, queryService, DTOptionsBuilder, DTColumnBuilder, 
     $scope.dtOptions = DTOptionsBuilder.newOptions()
     .withOption('ajax', {
         url: API_URL+'/apisql/datatable',
-        type: 'GET',
+        type: 'POST',
         headers: {
             "authorization":  'Basic ' + $localStorage.mediaToken
         },
@@ -104,6 +146,7 @@ function($scope, $state, $sce, queryService, DTOptionsBuilder, DTColumnBuilder, 
     .withPaginationType('full_numbers')
     .withOption('order', [0, 'desc'])
     .withDisplayLength(10)
+    .withOption('scrollX', true)
 
     .withOption('createdRow', $scope.createdRow);
 
@@ -114,10 +157,15 @@ function($scope, $state, $sce, queryService, DTOptionsBuilder, DTColumnBuilder, 
     }
     $scope.dtColumns.push(
         DTColumnBuilder.newColumn('code').withTitle('Code'),
-        DTColumnBuilder.newColumn('short_name').withTitle('Short Name'),
         DTColumnBuilder.newColumn('name').withTitle('Name'),
-        DTColumnBuilder.newColumn('description').withTitle('Description'),
-        DTColumnBuilder.newColumn('status_name').withTitle('Status')
+        DTColumnBuilder.newColumn('short_name').withTitle('Short Name'),
+        DTColumnBuilder.newColumn('account_type_name').withTitle('Type'),
+        DTColumnBuilder.newColumn('bank_account').withTitle('Bank Account'),
+        DTColumnBuilder.newColumn('bank_name').withTitle('Bank Name'),
+        DTColumnBuilder.newColumn('currency_name').withTitle('Currency'),
+        DTColumnBuilder.newColumn('gl_account_code').withTitle('GL Code'),
+        DTColumnBuilder.newColumn('ap_account_code').withTitle('AP Clearance'),
+        DTColumnBuilder.newColumn('ar_account_code').withTitle('AR Clearance'),
     );
 
     $scope.filter = function(type,event) {
@@ -151,23 +199,26 @@ function($scope, $state, $sce, queryService, DTOptionsBuilder, DTColumnBuilder, 
         // console.log($scope.contract)
         if ($scope.department.id.length==0){
             //exec creation
-
-            $scope.department.code = $scope.department.code;
-            $scope.department.name = $scope.department.name;
-            $scope.department.short_name = $scope.department.short_name;
-            $scope.department.description = $scope.department.description;
-            $scope.department.status = $scope.selected.dep.selected.id;
             var param = {
                 code: $scope.department.code,
                 name: $scope.department.name,
+                status: '1',
                 short_name: $scope.department.short_name,
                 description: $scope.department.description,
-                status: $scope.selected.dep.selected.id,
+                address1: $scope.department.address1,
+                address2: $scope.department.address2,
+                account_type: $scope.selected.account_type.selected.id,
+                bank_id: $scope.selected.bank.selected.id,
+                bank_account: $scope.department.bank_account,
+                currency_id: $scope.selected.currency.selected.id,
+                gl_account_id: $scope.selected.gl_account.selected.id,
+                ap_clearance_account_id: $scope.selected.ap_clearance.selected.id,
+                ar_clearance_account_id: $scope.selected.ar_clearance.selected.id,
                 created_date: globalFunction.currentDate(),
                 created_by: $localStorage.currentUser.name.id
             }
 
-            queryService.post('insert into mst_department SET ?',param)
+            queryService.post('insert into mst_cash_bank_account SET ?',param)
             .then(function (result){
                     $('#form-input').modal('hide')
                     $scope.dtInstance.reloadData(function(obj){
@@ -185,7 +236,7 @@ function($scope, $state, $sce, queryService, DTOptionsBuilder, DTColumnBuilder, 
             function (err){
                 $('#form-input').pgNotification({
                     style: 'flip',
-                    message: 'Error Insert: '+err.desc.code,
+                    message: 'Error Insert: '+err.code,
                     position: 'top-right',
                     timeout: 2000,
                     type: 'danger'
@@ -195,21 +246,26 @@ function($scope, $state, $sce, queryService, DTOptionsBuilder, DTColumnBuilder, 
         }
         else {
             //exec update
-            $scope.department.name = $scope.department.name;
-            $scope.department.short_name = $scope.department.short_name;
-            $scope.department.description = $scope.department.description;
-            $scope.department.status = $scope.selected.dep.selected.id;
             var param = {
                 code: $scope.department.code,
                 name: $scope.department.name,
+                status: '1',
                 short_name: $scope.department.short_name,
                 description: $scope.department.description,
-                status: $scope.selected.dep.selected.id,
+                address1: $scope.department.address1,
+                address2: $scope.department.address2,
+                account_type: $scope.selected.account_type.selected.id,
+                bank_id: $scope.selected.bank.selected.id,
+                bank_account: $scope.department.bank_account,
+                currency_id: $scope.selected.currency.selected.id,
+                gl_account_id: $scope.selected.gl_account.selected.id,
+                ap_clearance_account_id: $scope.selected.ap_clearance.selected.id,
+                ar_clearance_account_id: $scope.selected.ar_clearance.selected.id,
                 modified_date: globalFunction.currentDate(),
                 modified_by: $localStorage.currentUser.name.id
             }
             console.log(param)
-            queryService.post('update mst_department SET ? WHERE id='+$scope.department.id ,param)
+            queryService.post('update mst_cash_bank_account SET ? WHERE id='+$scope.department.id ,param)
             .then(function (result){
                 if (result.status = "200"){
                     console.log('Success Update')
@@ -235,10 +291,26 @@ function($scope, $state, $sce, queryService, DTOptionsBuilder, DTColumnBuilder, 
             $scope.department.id = result.data[0].id
             $scope.department.code = result.data[0].code
             $scope.department.name = result.data[0].name
+            $scope.department.address1 = result.data[0].address1
+            $scope.department.address2 = result.data[0].address2
+            $scope.department.bank_account = result.data[0].bank_account
             $scope.department.short_name = result.data[0].short_name
             $scope.department.description = result.data[0].description
             $scope.department.status = result.data[0].status
-            $scope.selected.dep.selected = {name: result.data[0].status == 1 ? 'Yes' : 'No' , id: result.data[0].status}
+            $scope.selected.account_type.selected = {name: result.data[0].account_type_name , id: result.data[0].account_type}
+            $scope.selected.bank.selected = {name: result.data[0].bank_name , id: result.data[0].bank_id}
+            $scope.selected.currency.selected = {name: result.data[0].currency_name , id: result.data[0].currency_id}
+            for (var i=0;i<$scope.gl_account.length;i++){
+                if ($scope.gl_account[i].id == result.data[0].gl_account_id){
+                    $scope.selected.gl_account.selected = $scope.gl_account[i]
+                }
+                if ($scope.gl_account[i].id == result.data[0].ap_clearance_account_id){
+                    $scope.selected.ap_clearance.selected = $scope.gl_account[i]
+                }
+                if ($scope.gl_account[i].id == result.data[0].ar_clearance_account_id){
+                    $scope.selected.ar_clearance.selected = $scope.gl_account[i]
+                }
+            }
 
         })
 
@@ -254,7 +326,7 @@ function($scope, $state, $sce, queryService, DTOptionsBuilder, DTColumnBuilder, 
     }
 
     $scope.execDelete = function(){
-        queryService.post('update mst_department SET status=\'2\' ,'+
+        queryService.post('update mst_cash_bank_account SET status=\'2\' ,'+
         ' modified_by='+$localStorage.currentUser.name.id+', ' +
         ' modified_date=\''+globalFunction.currentDate()+'\' ' +
         ' WHERE id='+$scope.department.id ,undefined)
