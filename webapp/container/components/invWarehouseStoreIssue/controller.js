948bf2cd2b4e8d6c8ebfe89413868c0b819572b0
ricky yuliadi <ricky.yuliadi@gmail.com>
2017-05-09 14:43:28 +0700

    add store

--- webapp/container/components/invWarehouseStoreIssue/controller.js
+++ webapp/container/components/invWarehouseStoreIssue/controller.js
@@ -19,8 +19,9 @@ function($scope, $state, $sce, productCategoryService, queryService, DTOptionsBu
         'where a.request_status=c.value '+
         'and a.issued_status=b.value '+
         'and a.origin_warehouse_id=d.id '+
-        'and a.dest_cost_center_id=e.id '
-    var qstringdetail = 'select a.id p_id,a.product_id ,b.name product_name,d.stock_qty,d.stock_qty_l stock_in_hand,a.request_qty,e.name unit_name,a.issued_qty,a.issued_status,f.name issued_status,b.unit_type_id unit_id,b.lowest_unit_type_id unit_id2,b.lowest_unit_conversion unit_conversion,d.id warehouse_item_id,b.recipe_unit_conversion '+
+        'and a.dest_cost_center_id=e.id '+
+		'and a.request_status!=0 '
+    var qstringdetail = 'select a.request_notes as item_notes,a.id p_id,a.product_id ,b.name product_name,d.stock_qty,d.stock_qty_l stock_in_hand,a.request_qty,e.name unit_name,a.issued_qty,a.issued_status,f.name issued_status,b.unit_type_id unit_id,b.lowest_unit_type_id unit_id2,b.lowest_unit_conversion unit_conversion,d.id warehouse_item_id,b.recipe_unit_conversion '+
         'from inv_store_req_line_item a,mst_product b,inv_store_request c,inv_warehouse_stock d,ref_product_unit e,(select value,name from table_ref where table_name=\'inv_store_req_line_item\')f '+
         'where a.product_id=b.id '+
         'and a.sr_id=c.id '+
@@ -66,7 +67,6 @@ function($scope, $state, $sce, productCategoryService, queryService, DTOptionsBu
         $scope.cost_center = data.data
     })
     $scope.costCenterUp = function(text){
-        console.log(text)
         queryService.post('select a.id, a.code,a.name,a.status,b.name as department_name, concat(\'Department: \',b.name)  dept_desc '+
             'from mst_cost_center a, mst_department b '+
             'where a.department_id = b.id '+
@@ -75,7 +75,6 @@ function($scope, $state, $sce, productCategoryService, queryService, DTOptionsBu
         .then(function(data){
             $scope.cost_center = data.data
         })
-
     }
     $scope.warehouse = []
     queryService.get('select id,name from mst_warehouse order by name',undefined)
@@ -194,7 +193,6 @@ function($scope, $state, $sce, productCategoryService, queryService, DTOptionsBu
             }, false)
         }
     }
-
     /*END AD ServerSide*/
 
     $scope.openQuickView = function(state){
@@ -203,13 +201,23 @@ function($scope, $state, $sce, productCategoryService, queryService, DTOptionsBu
         }
         $scope.items = []
         $scope.itemsOri = []
-
         $('#form-input').modal('show')
+		var dt = new Date()
+		$scope.ym = dt.getFullYear() + '/' + (dt.getMonth()<9?'0':'') + (dt.getMonth()+1)
+        //queryService.post('select cast(concat(\'PR/\',date_format(date(now()),\'%Y/%m/%d\'), \'/\', lpad(seq(\'PR\',\''+ym+'\'),4,\'0\')) as char) as code ',undefined)
+        queryService.post('select curr_document_no(\'SR\',\''+$scope.ym+'\') as code',undefined)
+        .then(function(data){
+            $scope.sr.code = data.data[0].code
+        })
     }
 
     $scope.submit = function(){
 		if ($scope.sr.id.length==0){
             //exec creation
+			queryService.post('select next_document_no(\'PR\',\''+$scope.ym+'\') as code',undefined)
+			.then(function(data){
+				$scope.sr.code = data.data[0].code
+			})
             var param = {
                 code: $scope.sr.code,
                 required_date: $scope.sr.date,
@@ -261,7 +269,6 @@ function($scope, $state, $sce, productCategoryService, queryService, DTOptionsBu
                     type: 'danger'
                 }).show();
             })
-
         }
         else {
             //exec update
@@ -269,6 +276,7 @@ function($scope, $state, $sce, productCategoryService, queryService, DTOptionsBu
                 code: $scope.sr.code,
                 required_date: $scope.sr.date,
                 request_status: $scope.selected.request_status.selected.id,
+				issued_status: $scope.selected.issued_status.selected.id,
                 request_notes: $scope.request_notes,
                 origin_warehouse_id: $scope.selected.warehouse.selected.id,
                 dest_cost_center_id: $scope.selected.cost_center.selected.id,
@@ -278,22 +286,33 @@ function($scope, $state, $sce, productCategoryService, queryService, DTOptionsBu
             queryService.post('update inv_store_request set ? where id='+$scope.sr.id,param)
             .then(function (result){
                 var qstr = $scope.child.saveTable($scope.sr.id)
-                queryService.post(qstr.join(';'),undefined)
-                .then(function (result2){
-                    $('#form-input').modal('hide')
-                    $scope.dtInstance.reloadData(function(obj){}, false)
-                    $('body').pgNotification({
-                        style: 'flip',
-                        message: 'Success Insert '+$scope.sr.code,
-                        position: 'top-right',
-                        timeout: 2000,
-                        type: 'success'
-                    }).show();
-                },
-                function (err2){
-                    console.log(err2)
-                })
-
+				if(qstr.length>1){
+	                queryService.post(qstr.join(';'),undefined)
+	                .then(function (result2){
+	                    $('#form-input').modal('hide')
+	                    $scope.dtInstance.reloadData(function(obj){}, false)
+	                    $('body').pgNotification({
+	                        style: 'flip',
+	                        message: 'Success Insert '+$scope.sr.code,
+	                        position: 'top-right',
+	                        timeout: 2000,
+	                        type: 'success'
+	                    }).show();
+	                },
+	                function (err2){
+	                    console.log(err2)
+	                })
+				}else{
+					$('#form-input').modal('hide')
+					$scope.dtInstance.reloadData(function(obj){}, false)
+					$('body').pgNotification({
+						style: 'flip',
+						message: 'Success Insert '+$scope.sr.code,
+						position: 'top-right',
+						timeout: 2000,
+						type: 'success'
+					}).show();
+				}
             },
             function (err){
                 $('#form-input').pgNotification({
@@ -317,7 +336,8 @@ function($scope, $state, $sce, productCategoryService, queryService, DTOptionsBu
             $scope.selected.warehouse['selected'] = {id:$scope.sr.origin_warehouse_id,name:$scope.sr.warehouse_name}
             $scope.selected.request_status['selected'] = {id:$scope.sr.request_status,name:$scope.sr.request_status_name}
             $scope.selected.issued_status['selected'] = {id:$scope.sr.issued_status,name:$scope.sr.issued_status_name}
-            queryService.post(qstringdetail+ ' and c.id='+ids,undefined)
+			console.log($scope.selected.issued_status)
+			queryService.post(qstringdetail+ ' and c.id='+ids,undefined)
             .then(function(result2){
                 $('#form-input').modal('show');
                 $scope.items = []
@@ -396,9 +416,6 @@ function($scope, $state, $sce, productCategoryService, queryService, DTOptionsBu
             status: ''
         }
     }
-
-
-
 })
 
 .controller('EditableTableSrCtrl', function($scope, $filter, $http, $q, queryService,$sce,$localStorage,globalFunction) {
@@ -430,7 +447,6 @@ function($scope, $state, $sce, productCategoryService, queryService, DTOptionsBu
 
     // mark user as deleted
     $scope.deleteUser = function(id) {
-        console.log(id)
         var filtered = $filter('filter')($scope.items, {id: id});
         if (filtered.length) {
             filtered[0].isDeleted = true;
@@ -452,10 +468,10 @@ function($scope, $state, $sce, productCategoryService, queryService, DTOptionsBu
             issued_qty: '',
             issued_id: '',
             issued_status: '',
+			request_notes: '',
             isNew: true
         };
         $scope.items.push($scope.item)
-        console.log($scope.items)
         var ss = 'select a.id,a.product_id,b.name product_name,a.stock_qty_l,b.lowest_unit_type_id unit_id,c.name unit_name '+
             'from inv_warehouse_stock a,mst_product b,ref_product_unit c '+
             'where a.product_id=b.id '+
@@ -465,7 +481,6 @@ function($scope, $state, $sce, productCategoryService, queryService, DTOptionsBu
             ' order by id limit 50 '
         queryService.post(ss,undefined)
         .then(function(data){
-            console.log(data.data)
             $scope.products = data.data
         })
     };
@@ -491,6 +506,7 @@ function($scope, $state, $sce, productCategoryService, queryService, DTOptionsBu
         var sqlitem = []
         for (var i = $scope.items.length; i--;) {
             var user = $scope.items[i];
+			console.log(user)
             // actually delete user
             /*if (user.isDeleted) {
                 $scope.items.splice(i, 1);
@@ -503,8 +519,8 @@ function($scope, $state, $sce, productCategoryService, queryService, DTOptionsBu
             // send on server
             //results.push($http.post('/saveUser', user));
             if (user.isNew && !user.isDeleted){
-                sqlitem.push('insert into inv_store_req_line_item(sr_id,product_id,request_qty,issued_qty,created_by) values '+
-                    '( '+sr_id+', '+user.product_id+','+parseInt(user.request_qty)+', '+parseInt(user.issued_qty_n)+', '+$localStorage.currentUser.name.id+' )')
+                sqlitem.push('insert into inv_store_req_line_item(sr_id,product_id,request_qty,issued_qty,created_by,request_notes) values '+
+                    '( '+sr_id+', '+user.product_id+','+parseInt(user.request_qty)+', '+parseInt(user.issued_qty_n)+', '+$localStorage.currentUser.name.id+' ,"'+user.item_notes+'")')
                 //sqlitem.push('insert into inv_pr_line_item (pr_id,product_id,'+(user.supplier_id.length>0?'supplier_id,':'')+'order_qty,net_price,order_amount,created_by,created_date) values('+
                 //pr_id+','+user.product_id+','+(user.supplier_id.length>0?user.supplier_id+',':'')+''+user.qty+','+user.price+','+user.amount+','+$localStorage.currentUser.name.id+','+'\''+globalFunction.currentDate()+'\''+')')
             }
@@ -514,49 +530,49 @@ function($scope, $state, $sce, productCategoryService, queryService, DTOptionsBu
             else if(!user.isNew){
                 for (var j=0;j<$scope.itemsOri.length;j++){
                     if ($scope.itemsOri[j].p_id==user.p_id){
-                        var d1 = $scope.itemsOri[j].p_id+$scope.itemsOri[j].product_id+$scope.itemsOri[j].request_qty+$scope.itemsOri[j].issued_qty_n
-                        var d2 = user.p_id+user.product_id+user.request_qty+user.issued_qty_n
+                        var d1 = $scope.itemsOri[j].p_id+$scope.itemsOri[j].product_id+$scope.itemsOri[j].request_qty+$scope.itemsOri[j].issued_qty_n+$scope.itemsOri[j].item_notes
+                        var d2 = user.p_id+user.product_id+user.request_qty+user.issued_qty_n+user.item_notes
                         if(d1 != d2){
                             var sql1 = 'update inv_store_req_line_item set '+
-                                'issued_qty = '+user.issued_qty_n+','+
-                                'issued_status = '+(parseInt(user.request_qty)>parseInt(user.issued_qty_n)?1:2)+','+
+                                'issued_qty = '+(parseInt(user.issued_qty)+parseInt(user.issued_qty_n))+','+
+                                'request_notes = "'+user.item_notes+'",'+
                                 ' modified_by = '+$localStorage.currentUser.name.id+',' +
                                 ' modified_date = \''+globalFunction.currentDate()+'\'' +
                                 ' where id='+user.p_id
-                            var sql2 = 'insert into inv_wh_stock_move(transc_type,sr_id,origin_warehouse_id,dest_cost_center_id,product_id,qty,unit_type_id,qty_l,lowest_unit_type_id,created_by) values '+
-                                '( \'SR\','+sr_id+','+$scope.selected.warehouse.selected.id+','+$scope.selected.cost_center.selected.id+','+
-                                ' '+user.product_id+', '+(user.issued_qty_n*user.unit_conversion)+', '+user.unit_id+','+user.issued_qty_n+','+user.unit_id2+', '+$localStorage.currentUser.name.id+' )'
-                            var sql3 = 'update inv_warehouse_stock set '+
-                                'warehouse_id = '+$scope.selected.warehouse.selected.id+','+
-                                'product_id = '+user.product_id+','+
-                                'stock_qty_l = stock_qty_l-'+((user.issued_qty_n*user.unit_conversion))+','+
-                                'stock_qty = stock_qty-'+(user.issued_qty_n)+','+
-                                ' modified_by = '+$localStorage.currentUser.name.id+',' +
-                                ' modified_date = \''+globalFunction.currentDate()+'\'' +
-                                ' where id='+user.warehouse_item_id
+							if(user.issued_qty_n>0){
+		                        var sql2 = 'insert into inv_wh_stock_move(transc_type,sr_id,origin_warehouse_id,dest_cost_center_id,product_id,qty,unit_type_id,qty_l,lowest_unit_type_id,created_by) values '+
+		                            '( \'SR\','+sr_id+','+$scope.selected.warehouse.selected.id+','+$scope.selected.cost_center.selected.id+','+
+		                            ' '+user.product_id+', '+(user.issued_qty_n*user.unit_conversion)+', '+user.unit_id+','+user.issued_qty_n+','+user.unit_id2+', '+$localStorage.currentUser.name.id+' )'
+		                        var sql3 = 'update inv_warehouse_stock set '+
+		                            'warehouse_id = '+$scope.selected.warehouse.selected.id+','+
+		                            'product_id = '+user.product_id+','+
+		                            'stock_qty_l = stock_qty_l-'+((user.issued_qty_n*user.unit_conversion))+','+
+		                            'stock_qty = stock_qty-'+(user.issued_qty_n)+','+
+		                            ' modified_by = '+$localStorage.currentUser.name.id+',' +
+		                            ' modified_date = \''+globalFunction.currentDate()+'\'' +
+		                            ' where id='+user.warehouse_item_id
 
-                            /*var sql4 = 'INSERT INTO inv_cost_center_stock(cost_center_id,product_id,stock_qty,stock_qty_l,stock_qty_in_recipe_unit,created_by) '+
-                                ' values('+$scope.selected.cost_center.selected.id+','+user.product_id+','+(user.stock_qty+(user.issued_qty_n*used.unit_conversion))+
-                                ','+(user.stock_qty_l+user.issued_qty_n)+', '+(user.stock_qty_in_recipe_unit+(user.issued_qty_n*user.recipe_unit_conversion))+','+$localStorage.currentUser.name.id+') ' +
-                                'ON DUPLICATE KEY UPDATE '+
-                                ' stock_qty = '+(user.stock_qty+(user.issued_qty_n*used.unit_conversion))+
-                                ' ,stock_qty_l = '+(user.stock_qty_l+user.issued_qty_n)
-                                ' ,stock_qty_in_recipe_unit = '+(user.stock_qty_in_recipe_unit+(user.issued_qty_n*user.recipe_unit_conversion))
-                                ' ,modified_by = '+$localStorage.currentUser.name.id+',' +
-                                ' modified_date = \''+globalFunction.currentDate()+'\''*/
+		                        var sql4 = 'INSERT INTO inv_cost_center_stock(cost_center_id,product_id,stock_qty,stock_qty_l,stock_qty_in_recipe_unit,created_by) '+
+		                            ' values('+$scope.selected.cost_center.selected.id+','+user.product_id+',(stock_qty+'+(user.issued_qty_n*user.unit_conversion)+')'+
+		                            ',stock_qty_l+'+user.issued_qty_n+', stock_qty_in_recipe_unit+'+(user.issued_qty_n*user.recipe_unit_conversion)+','+$localStorage.currentUser.name.id+') ' +
+		                            'ON DUPLICATE KEY UPDATE '+
+		                            ' stock_qty = (stock_qty+'+(user.issued_qty_n*user.unit_conversion)+')'+
+		                            ' ,stock_qty_l = (stock_qty_l+'+user.issued_qty_n+')'+
+		                            ' ,stock_qty_in_recipe_unit = (stock_qty_in_recipe_unit+'+(user.issued_qty_n*user.recipe_unit_conversion)+')'+
+		                            ' ,modified_by = '+$localStorage.currentUser.name.id+',' +
+		                            ' modified_date = \''+globalFunction.currentDate()+'\''
 
-                            sqlitem.push(sql1)
-                            sqlitem.push(sql2)
-                            sqlitem.push(sql3)
-                            //sqlitem.push(sql4)
+		                        sqlitem.push(sql1)
+		                        sqlitem.push(sql2)
+		                        sqlitem.push(sql3)
+		                        sqlitem.push(sql4)
+							}
                         }
                     }
                 }
             }
 
         }
-        console.log($scope.items)
-        console.log(sqlitem.join(';'))
         return sqlitem
         //return $q.all(results);
     };
@@ -577,32 +593,34 @@ function($scope, $state, $sce, productCategoryService, queryService, DTOptionsBu
             ' order by id limit 50 '
         queryService.post(ss,undefined)
         .then(function(data){
-            console.log(data.data)
             $scope.products = data.data
         })
     }
 
 
     $scope.getProductPrice = function(e,d){
-        console.log(e)
         $scope.items[d-1].product_id = e.product_id
         $scope.items[d-1].product_name = e.product_name
         $scope.items[d-1].stock_in_hand = e.stock_qty_l
         $scope.items[d-1].unit_id = e.unit_id
         $scope.items[d-1].unit_name = e.unit_name
-        console.log($scope.items)
-
     }
     $scope.updaterl = function(e,d,f,g){
-        console.log(f)
         if (g=='rq') {
             $scope.items[d-1].request_qty = f
         }
         else if (g=='iq') {
-            $scope.items[d-1].issued_qty_n = f
+			f=parseInt(f)
+			if($scope.items[d-1].stock_in_hand<$scope.items[d-1].issued_qty_n)
+				$scope.items[d-1].issued_qty_n=$scope.items[d-1].stock_in_hand
+			else if($scope.items[d-1].issued_qty_n+$scope.items[d-1].issued_qty>$scope.items[d-1].request_qty)
+				$scope.items[d-1].issued_qty_n=$scope.items[d-1].request_qty-$scope.items[d-1].issued_qty
+			else
+				$scope.items[d-1].issued_qty_n = f
         }
-
     }
-
+	$scope.update = function(e,d,f){
+		$scope.items[d-1].item_notes=f
+	}
 
 });
