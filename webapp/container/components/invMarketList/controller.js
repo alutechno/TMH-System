83c84dd4c00a7b59dec376cbdbdb9a70412d6781
ricky yuliadi <ricky.yuliadi@gmail.com>
2017-06-09 18:46:57 +0700

    update contract

--- webapp/container/components/invMarketList/controller.js
+++ webapp/container/components/invMarketList/controller.js
@@ -791,7 +791,7 @@ console.log($scope.seqState)
             $scope.pr.purchase_notes = result.data[0].ml_notes
             $scope.statusState = false
             $scope.doc_status = []
-            if (result.data[0].doc_status_id == 4 && && result.data[0].approval_status>0){
+            if (result.data[0].doc_status_id == 4 && result.data[0].approval_status>0){
                 $scope.releaseState = false
             }
             else $scope.releaseState = true
