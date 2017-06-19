12963a3284632cef3daf166d3f75b0ab6bc52bd3
ricky yuliadi <ricky.yuliadi@gmail.com>
2017-06-19 18:39:00 +0700

    report pos

--- webapp/container/components/posReports/controller.js
+++ webapp/container/components/posReports/controller.js
@@ -17,19 +17,19 @@ function($scope, $state, $sce, queryService, $localStorage, $compile, $rootScope
         $scope.report = data.data
 
 		for(var i=0;i<data.data.length;i++){
-			var qparam = `select report_file,a.name report,param1_name name,b.name type,param1_source source from mst_report_params a left join ref_param_type b
+			var qparam = `select report_file,a.name report,param1_name name,b.name type,param1_source source,param1_mandatory mandatory from mst_report_params a left join ref_param_type b
 				on a.param1_type_id=b.id
 				where a.id=`+data.data[i].id+` and param1_name is not null
 				union
-				select report_file,a.name report,param2_name name,b.name type,param2_source source from mst_report_params a left join ref_param_type b
+				select report_file,a.name report,param2_name name,b.name type,param2_source source,param2_mandatory mandatory from mst_report_params a left join ref_param_type b
 				on a.param2_type_id=b.id
 				where a.id=`+data.data[i].id+` and param2_name is not null
 				union
-				select report_file,a.name report,param3_name name,b.name type,param3_source source from mst_report_params a left join ref_param_type b
+				select report_file,a.name report,param3_name name,b.name type,param3_source source,param3_mandatory mandatory from mst_report_params a left join ref_param_type b
 				on a.param3_type_id=b.id
 				where a.id=`+data.data[i].id+` and param3_name is not null
 				union
-				select report_file,a.name report,param4_name name,b.name type,param4_source source from mst_report_params a left join ref_param_type b
+				select report_file,a.name report,param4_name name,b.name type,param4_source source,param4_mandatory mandatory from mst_report_params a left join ref_param_type b
 				on a.param4_type_id=b.id
 				where a.id=`+data.data[i].id+` and param4_name is not null`;
 			queryService.post(qparam,undefined)
